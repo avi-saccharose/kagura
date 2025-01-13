77c77afade8560e2918d8daea96bd91d626d6ffe
avi-saccharose <avi.vivi@aol.com>
2025-01-13 23:51:05 +0530

    add column to error

--- src/error.rs
+++ src/error.rs
@@ -4,8 +4,9 @@ use std::error::Error;
 #[derive(Debug, PartialEq, Clone)]
 pub(crate) struct KaguError {
     pub(crate) msg: String,
-    pub(crate) line: usize,
+    pub(crate) line: u32,
     pub(crate) start: usize,
+    pub(crate) column: u32,
     pub(crate) error_type: ErrorType,
 }
 
@@ -21,6 +22,6 @@ impl Error for KaguError {}
 
 impl fmt::Display for KaguError {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        f.write_fmt(format_args!("[line: ] {}", self.msg))
+        f.write_fmt(format_args!("[line: {}] {}", self.line, self.msg))
     }
 }
